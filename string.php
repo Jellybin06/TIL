a3023411820de73ed90f2fe4f67048481c087ac7
Jellybin06 <pjbboseung0817@gmail.com>
2022-08-07 13:41:22 +0900

    08-07-commmit

---
+++ string.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<body>
+  <h1>String & String Operator</h1>
+  <?php
+  echo 'Hello world';
+  //echo "Hello \"w\"orld";
+  //echo "Hello world";
+  ?>
+  <h2>concatenation operator</h2>
+  <?php
+  echo "Hello "."world";
+  //echo "Hello""world"; //error
+  ?>
+  <h2>String elngth function</h2>
+  <?php
+  echo strlen('hello world'); //11
+  ?>
+
+</body>
+</html>
