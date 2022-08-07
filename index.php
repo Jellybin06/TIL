16ec199f994bafb8b1650eb33aad63853b57f682
Jellybin06 <pjbboseung0817@gmail.com>
2022-08-07 20:26:30 +0900

    08-07-commit

--- index.php
+++ index.php
@@ -5,19 +5,28 @@
     <title></title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
       <li><a href="index.php?id=HTML">HTML</a></li>
       <li><a href="index.php?id=CSS">CSS</a></li>
       <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+      <li><a href="index.php?id=PHP">PHP</a></li>
     </ol>
     <h2>
       <?php
-        echo $_GET['id'];
+        if(isset($_GET['id'])){ //id가 있다면 true
+          echo $_GET['id'];
+        } else {
+          echo "Welcome";
+        }
       ?>
     </h2>
     <?php
-      echo  file_get_contents("data/".$_GET['id']);
+      if(isset($_GET['id'])){
+        echo file_get_contents("data/".$_GET['id']);
+      } else {
+        echo "Hello, PHP";
+      }
     ?>
   </body>
 </html>
