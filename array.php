4aa4e1a5b130e6323518c67696b06cff134ed802
Jellybin06 <pjbboseung0817@gmail.com>
2022-08-08 14:28:18 +0900

    08-08-commit

---
+++ array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Array</title>
+  </head>
+  <body>
+    <h1>Array</h1>
+    <?php
+      $coworkers = array('egoing', 'leezche', 'duru', 'jelly');
+      echo $coworkers[1].'<br>';
+      echo $coworkers[3].'<br>';
+      echo count($coworkers);
+      //var_dump(count($coworkers)); //int(4)
+      array_push($coworkers, 'graphittie');
+      var_dump($coworkers);
+    ?>
+  </body>
+</html>
