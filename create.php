43899117b4f264a7b7ebbec379b9fbaf09cc4635
Jellybin06 <pjbboseung0817@gmail.com>
2022-08-11 19:00:15 +0900

    08-11-commit

--- create.php
+++ create.php
@@ -46,16 +46,16 @@
       ?>
     </ol>
     <a href="create.php">create</a>
-    <?php if(isset($_GET['id'])) { ?>
-      <a href="update.php/id=<?php echo $_GET['id'];//<?=$_GET['id']; ?>">update</a>
-    <?php } ?>
-    <h2>
-      <?php
-        print_title();
-      ?>
-    </h2>
-    <?php
-      print_description();
-    ?>
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" placeholder="Description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
   </body>
 </html>
--- update.php
+++ update.php
@@ -50,12 +50,17 @@
       <a href="update.php/id=<?php echo $_GET['id'];//<?=$_GET['id']; ?>">update</a>
     <?php } ?>
     <h2>
-      <?php
-        print_title();
-      ?>
-    </h2>
-    <?php
-      print_description();
-    ?>
+
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title" value="<?php print_title(); ?>">
+      </p>
+      <p>
+        <textarea name="description" placeholder="Description" value=<?php print_description(); ?>></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
   </body>
 </html>
