95041851298219fad7c89daf9548316c88a65fcb
Jellybin06 <pjbboseung0817@gmail.com>
2022-08-13 19:33:53 +0900

    08-13-commit

--- variable.php
+++ variable.php
@@ -11,9 +11,10 @@
     //echo $a+1;
     $name = "egoing";
     echo "Lorem ipsum dolor sit amet, ".$name."consectetur adipisicing elit, ".$name."sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non egoingproident, sunt in culpa qui egoingofficia deserunt egoingmollit anim id est laborum.";
-    
+
     ?>
 
 
-  </body>
-</html>
+    <?php
+    require('view/bottom.php')
+     ?>
