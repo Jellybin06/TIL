d393cb3ec855626c081a1ce259d90f672f31a9fd
Jellybin06 <pjbboseung0817@gmail.com>
2022-08-22 13:18:02 +0900

    08-22-commit

---
+++ custom_module.js
@@ -0,0 +1,9 @@
+// exports 객체를 사용해서 sum이라는 변수를 만들고, sum 변수에 function 을 사용해서 피라미터를 가진 함수식을 대입
+exports.sum = function(max){
+    // 입력된 값을 최대값으로 1부터 최대값까지 더해서 반환하는 로직
+    return (max+1)*max/2;
+}
+
+// var 변수에 'NEW VALUE 100' 입력
+exports.var1 = 'NEW VALUE 100';
+
