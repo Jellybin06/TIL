2ecadd1540b5944cba3acba6319f2247ba536ed9
Jellybin <pjbboseung0817@gmail.com>
2022-10-06 20:03:47 +0900

    10-06-commit

--- mysql.sql
+++ mysql.sql
@@ -10,3 +10,13 @@ SHOW DATABASES;
 USE (DB이름);
 
 -- column 열 , table 표, row,record 행
+
+CREATE TABLE topic(
+    id INT(11) NOT NULL AUTO_INCREMENT, -- 11글자 이내, 공백이면 안됨, 자동으로 1씩 증가(중복되지 않는 식별자를 가지게됨)
+    title VARCHAR(100) NOT NULL, -- 100글자 이내, 공백이면 안됨
+    description TEXT NULL, -- text에 정해진 글자 이내, 공백 가능
+    created DATETIME NOT NULL, -- 날짜 , 공백 불가능
+    author VARCHAR(30) NULL, -- 30글자 이내 , 공백 가능
+    profile VARCHAR(100) NULL, -- 100글자 이내 , 공백 가능
+    PRIMARY KEY(id)); -- 기본키 (id)
+    
